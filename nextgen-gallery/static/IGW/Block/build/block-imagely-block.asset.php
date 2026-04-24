0a0a4d987d3df0746ae33b61fd13ff653ce61bfe
Sean Murphy <sean@iamseanmurphy.com>
2026-04-24 16:32:25 +0000

    4.2.0

--- nextgen-gallery/static/IGW/Block/build/block-imagely-block.asset.php
+++ nextgen-gallery/static/IGW/Block/build/block-imagely-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '1d0fa10bb42eabf24699');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b1a6f852d15034ab5611');
